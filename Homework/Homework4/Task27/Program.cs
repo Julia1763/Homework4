46de14502f2d23a237474f39382a914d95c45702
Julia <odintsova.julia@mail.ru>
2022-08-17 21:21:43 +0300

    Записали решение к задаче 27

--- Homework/Homework4/Task27/Program.cs
+++ Homework/Homework4/Task27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе. */
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int SumOfDigit(int N)
+{
+    int sum = 0;
+    while (N>0)
+    {
+      sum+=N%10;
+      N/=10;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumOfDigit(number));
