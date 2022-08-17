a051d7bb39b34b9673e2259a408a6a0b23cedca6
Julia <odintsova.julia@mail.ru>
2022-08-17 16:13:25 +0300

    Записали решение к 25 задаче

--- Homework/Homework4/Task25/Program.cs
+++ Homework/Homework4/Task25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+
+Console.WriteLine("Введите число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int numB = int.Parse(Console.ReadLine());
+int RaisedToThePower(int A, int B)
+{
+    int result=1;
+    for (int i = 1; i <=B; i++)
+    {
+      result*=A;
+    }
+    return result;
+}
+if (numB>0)
+{
+  Console.WriteLine(RaisedToThePower(numA, numB));
+}
+else
+{
+  Console.WriteLine("Степень числа введена некорректно");  
+}
