f9a63e32d36ce2ea40e3ac0f2e888daa46440d04
Julia <odintsova.julia@mail.ru>
2022-09-01 14:04:49 +0300

    Дописали решение к задаче 56

--- Desktop/Homework/Homework8/Task56/Program.cs
+++ Desktop/Homework/Homework8/Task56/Program.cs
@@ -76,15 +76,3 @@ return Min;
 }
 Console.WriteLine($"Сумма минимальной строки равна {MinOfRows(Matrix)}");
 
-/*int minSumLine = 0;
-int sumLine = SumLineElements(array, 0);
-for (int i = 1; i < array.GetLength(0); i++)
-{
-  int tempSumLine = SumLineElements(array, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}*/
-
