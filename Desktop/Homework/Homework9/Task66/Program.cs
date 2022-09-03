88b3139a783151bac73789cffe8620de51b02807
Julia <odintsova.julia@mail.ru>
2022-09-03 21:39:29 +0300

    Решили  задачу 66

--- Desktop/Homework/Homework9/Task66/Program.cs
+++ Desktop/Homework/Homework9/Task66/Program.cs
@@ -1,3 +1,19 @@
 ﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */ 
+
+int SumOfMN (int M, int N)
+{
+   if (M == N) {
+      return M;
+    } else if(M < N) {
+      return M + SumOfMN(M+1, N);
+    } else {
+      return N + SumOfMN(M, N+1);
+    }
+}
+Console.WriteLine("Введите первое число: ");
+int FnumNer = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int SnumNer = int.Parse(Console.ReadLine());
+Console.WriteLine(SumOfMN(FnumNer, SnumNer));
