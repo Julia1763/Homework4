a978e25e1f61bdef65672d60ade8f9fd8453245e
Julia <odintsova.julia@mail.ru>
2022-09-03 22:26:26 +0300

    Создали файл к задаче 68

---
+++ Desktop/Homework/Homework9/Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29 */
+
+int AckermannFunction (int m, int n)
+{
+if (m == 0) {
+      return n+1;
+    } else if(m > 0 && n ==0) {
+      return AckermannFunction(m-1, 1);
+    } else if(m > 0 && n > 0){
+      return AckermannFunction(m-1, AckermannFunction(m,n-1));
+    }
+}
+
+Console.WriteLine("Введите первое число: ");
+int Fnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int Snumber = int.Parse(Console.ReadLine());
+Console.WriteLine(AckermannFunction(Fnumber, Snumber));
