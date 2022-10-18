49e48b81b3b4838dc2758cc139cdaf2a022082bc
Julia <odintsova.julia@mail.ru>
2022-10-18 16:54:03 +0300

    Собрали

--- Desktop/Python/Phone_book/data_provider.py
+++ Desktop/Python/Phone_book/data_provider.py
@@ -6,31 +6,17 @@ Dict = {}
 
 def enter_name(information):
     name=input('Введите имя пользователя: ')
-    file = open("Text.bin", 'wb')
-    pickle.dump(Dict, file)
-    file.close()
-    exit_menu = input("Добавить ещё данные или выйти в меню?")
-    if exit_menu == 'Выйти' or exit_menu == 'выйти' or exit_menu == 'exit':
-        exit()
+    return name
+    
 
 def enter_surname(information):
     surname=input('Введите фамилию пользователя: ')
-    file = open("Text.bin", 'wb')
-    pickle.dump(Dict, file)
-    file.close()
-    exit_menu = input("Добавить ещё данные или выйти в меню?")
-    if exit_menu == 'Выйти' or exit_menu == 'выйти' or exit_menu == 'exit':
-        exit()
-
+    return surname
+   
 
 def enter_phone(information):
     phone_number=int(input('Введите фамилию пользователя: '))
-    file = open("Text.bin", 'wb')
-    pickle.dump(Dict, file)
-    file.close()
-    exit_menu = input("Добавить ещё данные или выйти в меню?")
-    if exit_menu == 'Выйти' or exit_menu == 'выйти' or exit_menu == 'exit':
-        exit()
+    return phone_number
 
 def data_collection(information = 1):
     return (enter_name(information), enter_surname(information), enter_phone(information))
